92eeae903ddfbb69e85e5035300501cec41cf8b4
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-17 22:39:14 -0300

    more debug

--- src/apps/Users.py
+++ src/apps/Users.py
@@ -17,7 +17,11 @@ class Users:
     def get(self, url, body, headers, queryParam):
         url = f"{self.host}{url}{getQueryParams(queryParam)}"
         print(f"url is {url}")
-        response = requests.get(url, json=body, headers=headers)
+        try:
+            response = requests.get(url, json=body, headers=headers)
+        except Exception as e:
+            message = {"url": url, "error": f"{e}"}
+            return make_response(message, 500)
         print(f"response is: {response} and json {response.json()}")
         return make_response(response.json(), response.status_code)
 
