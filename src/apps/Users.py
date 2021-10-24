22aa616ef5459b4364f4dba10b4c5b04bc05fa4c
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-24 03:12:23 -0300

    improve api gateway code

--- src/apps/Users.py
+++ src/apps/Users.py
@@ -16,23 +16,22 @@ class Users:
 
     def get(self, url, body, headers, queryParam):
         url = f"{self.host}{url}{getQueryParams(queryParam)}"
-        print(f"url is {url}")
-        try:
-            response = requests.get(url, json=body, headers=headers)
-        except Exception as e:
-            message = {"url": url, "error": f"{e}"}
-            return make_response(message, 500)
-        print(f"response is: {response} and json {response.json()}")
-        return make_response(response.json(), response.status_code)
+        response = requests.get(url, json=body, headers=headers)
+        return make_response(self.getResponseJson(response), response.status_code)
+
+    def getResponseJson(self, response):
+        if response.status_code == 503 or not response.text:
+            return {"message": "users service is currently unavailable, please try later", "status": 503}
+        return response.json()
 
     def post(self, url, body, headers, queryParam):
         response = requests.post(f"{self.host}{url}{getQueryParams(queryParam)}", json=body, headers=headers)
-        return make_response(response.json(), response.status_code)
+        return make_response(self.getResponseJson(response), response.status_code)
 
     def delete(self, url, body, headers, queryParam):
         response = requests.delete(f"{self.host}{url}{getQueryParams(queryParam)}", json=body, headers=headers)
-        return make_response(response.json(), response.status_code)
+        return make_response(self.getResponseJson(response), response.status_code)
 
     def patch(self, url, body, headers, queryParam):
         response = requests.patch(f"{self.host}{url}{getQueryParams(queryParam)}", json=body, headers=headers)
-        return make_response(response.json(), response.status_code)
+        return make_response(self.getResponseJson(response), response.status_code)
