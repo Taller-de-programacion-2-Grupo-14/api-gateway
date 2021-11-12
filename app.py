1ee9790cf83624130799c475c60be707b2110d23
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-11-12 00:19:31 -0300

    Add swagger to make the code work

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from werkzeug.routing import BaseConverter
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
+from werkzeug.routing import BaseConverter
+
 from src.Resource import Gateway
 
 app = Flask(__name__)
@@ -10,6 +11,19 @@ class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
+
+
+@app.after_request
+def _build_cors_post_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+
+
+@app.route("/doc")
+def getDoc():
+    return render_template("index.html")
 
 
 app.url_map.converters['regex'] = RegexConverter
