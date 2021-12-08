1f47cc780cfd72891110dc92d83c4f1b23983304
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-12-08 17:56:27 -0300

    add logger for heroku

--- src/apps/Courses.py
+++ src/apps/Courses.py
@@ -63,7 +63,9 @@ class Courses:
         body, shouldFinish = processHeader(headers, body)
         if shouldFinish:
             return make_response(body, body['status'])
-        response = requests.post(f"{self.host}{url}{getQueryParams(queryParam)}", json=body, headers=headers)
+        url = f"{self.host}{url}{getQueryParams(queryParam)}"
+        print(f"will do request with url: {url} and body {body}")
+        response = requests.post(url, json=body, headers=headers)
         return make_response(self.getResponseJson(response), response.status_code)
 
     def delete(self, url, body, headers, queryParam):
